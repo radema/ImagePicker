cd99c742c7bb3f90b371a4ab0764f40e1b408c5e
Raul De Maio <r.demaio@iconsulting.biz>
2020-09-20 00:09:32 +0200

    Added training notebook and trained model. Update main.py to add the model. If prediction > 0.76 then the image is moved to the last folder. Otherwise it is moved to the temporary one.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 
+from collections import Counter
 from os import listdir, stat
 from os.path import isfile, join
-from PIL import Image
+import PIL.Image as Image
+import numpy as np
 
 import shutil
 from local_credentials import *
 
+import tensorflow as tf
+from tensorflow.keras import models
 
+model = models.load_model(model_folder)
+print("Model Loaded!")
 
+print("Reading images...")
 # files name in spotlight images folder
 files = [f for f in listdir(spotlight_folder) if isfile(join(spotlight_folder, f)) 
 	and stat(join(spotlight_folder, f)).st_size/(1024)>100]
@@ -15,10 +22,26 @@ files = [f for f in listdir(spotlight_folder) if isfile(join(spotlight_folder, f
 #files name of images already present in Immagini
 images = [f for f in listdir(destination_folder) if isfile(join(destination_folder, f))]
 
+counter= Counter()
+
 for file in files:
 	target = join(destination_folder,file+'.jpg')
 	im = Image.open(join(spotlight_folder,file))
 	w,h = im.size
 	# we move only not cropped images
 	if w>h:
-		shutil.copyfile(join(spotlight_folder,file), target)
+		print("Detected image")
+		im = np.array(im.resize((224,224)))/255.0
+		result = model.predict(im[np.newaxis, ...])[0,0]
+		if 0.34 < result <0.76:
+			shutil.copyfile(join(spotlight_folder,file), target)
+			counter.update(["uncertainty"])
+		elif result >= 0.76:
+			counter.update(['good'])
+			#print("Test!")
+			shutil.copyfile(join(spotlight_folder,file),join(picture_folder,file+'.jpg') )
+		else:
+			counter.update(['bad'])
+print("\n")
+print(counter)
+print("\n")
